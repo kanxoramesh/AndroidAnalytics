10463b05e295accb53fbad97810c22ab7d0e0634
kanxoramesh <pokhrelramesh1996@gmail.com>
2024-06-26 13:12:03 +0545

    initialize analytics SDK

---
+++ app/src/main/java/com/example/myapplication/MyApplication.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import AndroidAnalytics
+import android.app.Application
+import com.analytics.analytics_android.Logger
+import com.analytics.analytics_android.core.AnalyticsLogger
+import com.analytics.analytics_android.utils.LogLevel
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidAnalytics.initialize(
+            AndroidAnalytics.Builder(this)
+                .setStorage(AnalyticsStorage(this))
+                .setLogLevel(LogLevel.INFO)
+        )
+    }
+}
