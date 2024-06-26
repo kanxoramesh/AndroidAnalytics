238b51f5f98ba645bc47f94080628bee71860999
kanxoramesh <pokhrelramesh1996@gmail.com>
2024-06-26 13:12:26 +0545

    update analytics sdk

--- analytics-android/src/main/java/com/analytics/analytics_android/AndroidAnalytics.kt
+++ analytics-android/src/main/java/com/analytics/analytics_android/AndroidAnalytics.kt
@@ -1,46 +1,88 @@
+import android.content.Context
+import android.util.Log
 import com.analytics.analytics_android.AnalyticsEvent
-import com.analytics.analytics_android.AnalyticsSession
-import com.analytics.analytics_android.AnalyticsStorage
+import com.analytics.analytics_android.Logger
+import com.analytics.analytics_android.Storage
+import com.analytics.analytics_android.core.AnalyticsLogger
+import com.analytics.analytics_android.core.AnalyticsSession
+import com.analytics.analytics_android.utils.LogLevel
 
 class AndroidAnalytics private constructor(
-    private val storage: AnalyticsStorage
+    private val builder: Builder
 ) {
     private var currentSession: AnalyticsSession? = null
+    private var logger: Logger? = AnalyticsLogger.with(LogLevel.INFO)
+
+    init {
+        logger = AnalyticsLogger.with(builder.getLogLevel());
+    }
+
+    companion object {
+        private var instance: AndroidAnalytics? = null
+
+        fun getInstance(): AndroidAnalytics {
+            return instance
+                ?: throw IllegalStateException("DefaultAnalyticsManager must be initialized using the Builder before getting the instance.")
+        }
+
+        fun initialize(builder: Builder) {
+            if (instance == null) {
+                instance = builder.build()
+            }
+        }
+    }
 
     fun startSession() {
         if (currentSession == null) {
             currentSession = AnalyticsSession()
-            println("Session started: ${currentSession?.sessionId}")
+            logger?.info("Session started: ${currentSession?.sessionId}")
+        }
+    }
+
+    fun addEvent(eventName: String, properties: Map<String, Any>) {
+        currentSession?.let {
+            it.addEvent(AnalyticsEvent(eventName, properties))
+            logger?.info("Event added: $eventName with properties: $properties")
+        } ?: {
+            startSession()
+            currentSession?.addEvent(AnalyticsEvent(eventName, properties))
         }
     }
 
     fun endSession() {
         currentSession?.let {
             it.endSession()
-            storage.saveSession(it)
-            println("Session ended: ${it.sessionId}")
+            builder.getStorage()?.saveSession(it)
+            logger?.info("Session ended: ${it.sessionId}")
             currentSession = null
         }
     }
 
-    fun addEvent(eventName: String, properties: Map<String, Any>) {
-        currentSession?.let {
-            val event = AnalyticsEvent(eventName, properties)
-            it.addEvent(event)
-            println("Event added: $eventName with properties: $properties")
-        }
+    fun getSessionsData() {
+        builder.getStorage()?.getSessions()
     }
 
-    class Builder {
-        private var storage: AnalyticsStorage? = null
 
-        fun setStorage(storage: AnalyticsStorage) = apply { this.storage = storage }
+    class Builder(private val context: Context) {
+        private var storage: Storage? = null
+        private var logLevel: LogLevel? = null
+
+
+        fun setStorage(storage: Storage) = apply { this.storage = storage }
+        fun getStorage(): Storage? {
+            return storage
+        }
+
+        fun setLogLevel(logLevel: LogLevel) = apply { this.logLevel = logLevel }
+        fun getLogLevel(): LogLevel? {
+            return logLevel
+        }
 
         fun build(): AndroidAnalytics {
             if (storage == null) {
                 throw IllegalArgumentException("Storage must be set")
             }
-            return AndroidAnalytics(storage!!)
+            return AndroidAnalytics(this)
         }
     }
 }
